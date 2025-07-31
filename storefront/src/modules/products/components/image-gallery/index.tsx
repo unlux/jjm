6cd3425025531c158bf17949706b7c2cd1e7c966
lakshay choudhary <lakshaychoudhary77712@gmail.com>
2025-08-01 00:25:55 +0530

    add zoom lens in store image gallery

--- storefront/src/modules/products/components/image-gallery/index.tsx
+++ storefront/src/modules/products/components/image-gallery/index.tsx
@@ -1,7 +1,7 @@
 import { HttpTypes } from "@medusajs/types"
 import { Container } from "@medusajs/ui"
 import Image from "next/image"
-
+import { Lens } from "./Lens"
 type ImageGalleryProps = {
   images: HttpTypes.StoreProductImage[]
 }
@@ -12,25 +12,27 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
       <div className="flex flex-col flex-1 small:mx-16 gap-y-4">
         {images.map((image, index) => {
           return (
-            <Container
-              key={image.id}
-              className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
-              id={image.id}
-            >
-              {!!image.url && (
-                <Image
-                  src={image.url}
-                  priority={index <= 2 ? true : false}
-                  className="absolute inset-0 rounded-rounded"
-                  alt={`Product image ${index + 1}`}
-                  fill
-                  sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
-                  style={{
-                    objectFit: "cover",
-                  }}
-                />
-              )}
-            </Container>
+            <Lens key={image.id}>
+              <Container
+                key={image.id}
+                className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
+                id={image.id}
+              >
+                {!!image.url && (
+                  <Image
+                    src={image.url}
+                    priority={index <= 2 ? true : false}
+                    className="absolute inset-0 rounded-rounded"
+                    alt={`Product image ${index + 1}`}
+                    fill
+                    sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
+                )}
+              </Container>
+            </Lens>
           )
         })}
       </div>
