308345e17cffc235e8f3d0a97bf1b386aab90ef5
lakshay choudhary <lakshaychoudhary77712@gmail.com>
2025-08-17 20:17:02 +0530

    fix: hero slider height

--- storefront/src/modules/home/components/hero/HeroSlider.tsx
+++ storefront/src/modules/home/components/hero/HeroSlider.tsx
@@ -1,27 +1,24 @@
-"use client";
-import { useRef } from "react";
-import { useRouter } from "next/navigation";
+"use client"
+import { useRef } from "react"
+import { useRouter } from "next/navigation"
 
 export default function HeroSlider() {
-    const videoRef = useRef<HTMLVideoElement>(null);
-    const router = useRouter();
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const router = useRouter()
 
-
-    return (
-        <div className="relative w-full h-[65vh] md:h-[75vh] lg:h-[88.18vh] overflow-hidden">
-            <div className="relative h-full w-full">
-                <video
-                    ref={videoRef}
-                    src="/outputx.mp4"
-                    className="absolute inset-0 w-full h-full object-cover brightness-[0.9]"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                    style={{ cursor: "pointer" }}
-                />
-                <div className="absolute inset-0 animate-fadeIn z-30" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="relative w-full overflow-hidden">
+      <video
+        ref={videoRef}
+        src="/outputx.mp4"
+        className="w-full h-auto object-cover brightness-[0.9]"
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{ cursor: "pointer" }}
+      />
+      <div className="absolute inset-0 animate-fadeIn z-30" />
+    </div>
+  )
 }
